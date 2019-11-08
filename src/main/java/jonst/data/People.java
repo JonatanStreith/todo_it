6cbc9ca71a79f8e12a5c1b8adb5d98b762674dae
JonatanStreith <jonatan@streith.se>
2019-11-08 15:08:26 +0100

    People class done

---
+++ src/main/java/jonst/data/People.java
@@ -0,0 +1,61 @@
+package jonst.data;
+
+import jonst.model.Person;
+
+import java.util.Arrays;
+
+public class People {
+
+    private static Person[] peopleArray = new Person[0];
+
+
+    public int size() {
+
+        return peopleArray.length;
+    }
+
+
+    public Person[] findAll() {
+
+        return peopleArray;
+    }
+
+    public Person findById(int personId) {
+
+
+        for (Person pers : peopleArray) {
+            if(pers.getPersonId() == personId)
+                return pers;
+
+        }
+
+
+        //int indexOf = Arrays.binarySearch(peopleArray, personId);       //Assuming the sequencer function is used, the array will be sorted, so binarySearch works
+
+        return peopleArray[0];        //POTENTIAL RISK: If nonexistent Id is given, proper return won't work and error will happen. Address later.
+    }                                       //Perhaps function that checks if an Id is valid, and use it before trying to return a Person?
+
+    public Person AddPerson(String firstName, String lastName) {
+
+        Person newGuy = new Person(PersonSequencer.nextPersonId(), firstName, lastName);    //Creates a new guy, using the nextPersonId to get a fresh number
+
+
+        peopleArray = Arrays.copyOf(peopleArray, peopleArray.length + 1);   //Increase the array size by one
+
+
+
+        peopleArray[peopleArray.length-1] = newGuy;   //Puts the new guy last in the array
+
+        return newGuy;
+    }
+
+
+    public void clear(){
+
+        peopleArray = new Person[0];
+        PersonSequencer.reset();        //We need to reset the Id counter if we clear the array, right?
+
+    }
+
+
+}
---
+++ src/test/java/jonst/data/PeopleTest.java
@@ -0,0 +1,104 @@
+package jonst.data;
+
+import jonst.model.Person;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+
+public class PeopleTest {
+
+
+    @Test
+    public void addPersonTest() {
+        People classA = new People();
+
+        String firstName = "Esther", lastName = "Bright";
+
+        Person result = classA.AddPerson(firstName, lastName);
+
+        assertNotNull(result);
+
+
+        assertEquals(1, result.getPersonId());
+        assertEquals("Esther", result.getFirstName());
+        assertEquals("Bright", result.getLastName());
+        assertEquals("Bright", classA.findAll()[0].getLastName());  //Can test that data has been added to the actual array, too
+
+    }
+
+    @Test
+    public void sizeTest() {
+        People classA = new People();
+
+
+        assertEquals(0, classA.size());
+
+
+        String firstName = "Esther", lastName = "Bright";
+
+        Person result = classA.AddPerson(firstName, lastName);
+
+        assertEquals(1, classA.size());         //We've created one entry, so we assume one is the right size
+
+    }
+
+    @Test
+    public void findAllTest() {
+        People classA = new People();
+
+
+        String firstName = "Esther", lastName = "Bright";
+
+        Person result = classA.AddPerson(firstName, lastName);
+
+        Person[] test = classA.findAll();
+
+        System.out.println(test.length);
+
+        assertNotNull(test);
+        assertEquals(1, test[0].getPersonId());
+        assertEquals("Esther", test[0].getFirstName());
+        assertEquals("Bright", test[0].getLastName());
+
+
+    }
+
+    @Test
+    public void findByIdTest() {
+        People classA = new People();
+        String firstName = "Esther", lastName = "Bright";
+
+        Person result = classA.AddPerson(firstName, lastName);
+
+        Person test = classA.findById(1);
+
+        assertNotNull(test);
+        assertEquals(1, test.getPersonId());
+        assertEquals("Esther", test.getFirstName());
+        assertEquals("Bright", test.getLastName());
+
+    }
+
+    @Test
+    public void clearTest() {
+        People classA = new People();
+
+        String firstName = "Esther", lastName = "Bright";
+
+        Person result = classA.AddPerson(firstName, lastName);
+
+
+        classA.clear();
+
+        Person[] test = classA.findAll();       //Get the now empty array
+
+        assertTrue(test.length == 0);   //See if that array is zero length
+
+    }
+
+}
