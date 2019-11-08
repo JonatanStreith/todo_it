bfab09ad1c7a358bf0bb3cb0ee70ec15e3aea84f
JonatanStreith <jonatan@streith.se>
2019-11-08 09:39:04 +0100

    Person class done

--- src/test/java/jonst/model/PersonTest.java
+++ src/test/java/jonst/model/PersonTest.java
@@ -1,33 +1,32 @@
 package jonst.model;
 
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Unit test for Person
  */
 public class PersonTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
 
 
     @Test
-    public void Person() {
+    public void PersonTest() {
 
         int personId = 555;
         String firstName = "Alex";
         String lastName = "Hawkwind";
 
         Person testCase = new Person(personId, firstName, lastName);
+
+        assertNotNull(testCase);
+
+        assertEquals(555, testCase.getPersonId());
+        assertEquals("Alex", testCase.getFirstName());
+        assertEquals("Hawkwind", testCase.getLastName());
 
     }
 }
